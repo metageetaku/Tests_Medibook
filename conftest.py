ae80127ebe7b99a1716c26463a5a70a3bd848090
Romain Roux <cyberromain1989@yahoo.fr>
2026-01-11 12:27:32 +0100

    Fix CI: geckodriver, Firefox headless, Docker Compose, artifact v4

--- conftest.py
+++ conftest.py
@@ -2,13 +2,13 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+from webdriver_manager.firefox import GeckoDriverManager
 
 @pytest.fixture(scope="function")
 def browser():
-    """Fixture ultra-simple pour Firefox"""
     options = Options()
-    service = Service(executable_path="C:\\Windows\\geckodriver.exe")
+    options.headless = True  
+    service = Service(GeckoDriverManager().install())
     driver = webdriver.Firefox(service=service, options=options)
-    driver.maximize_window()
     yield driver
     driver.quit()
