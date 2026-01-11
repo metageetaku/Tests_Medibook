eac5f9622dcbeaec6f553fd0f8370ad532246f2c
Romain Roux <cyberromain1989@yahoo.fr>
2026-01-11 14:36:35 +0100

    Test workflow 2

--- conftest.py
+++ conftest.py
@@ -1,15 +1,20 @@
 # conftest.py
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.firefox.options import Options
 import os
 
 @pytest.fixture
 def browser():
+    firefox_options = Options()
+    firefox_options.headless = True  # headless pour CI
+
     remote_url = os.environ.get("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+
+    # Correct pour Selenium 4+
     driver = webdriver.Remote(
         command_executor=remote_url,
-        desired_capabilities=DesiredCapabilities.FIREFOX
+        options=firefox_options
     )
     yield driver
     driver.quit()
