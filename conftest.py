367fb1a31ebb0e561c2e6e84c94eb4c0849c05f8
Romain Roux <cyberromain1989@yahoo.fr>
2026-01-11 13:50:15 +0100

    workplz7

--- conftest.py
+++ conftest.py
@@ -3,21 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
-@pytest.fixture(scope="function")
-def browser():
+@pytest.fixture
+def driver():
     options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1280,720")  # moins lourd pour CI
-
-    service = Service("/usr/local/bin/geckodriver")
+    options.headless = True  # True = pas de fenêtre, parfait pour CI
+    # Sur Windows GitHub runner, Firefox est déjà dans le PATH
+    service = Service()  # Selenium trouvera geckodriver automatiquement
     driver = webdriver.Firefox(service=service, options=options)
-
-    # Timeouts
-    driver.set_page_load_timeout(180)
-    driver.implicitly_wait(10)
-
     yield driver
     driver.quit()
