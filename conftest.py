74d4d24559cd7ba1a5b418bcd29bfc9d19833fca
Romain Roux <cyberromain1989@yahoo.fr>
2026-01-11 13:35:46 +0100

    workplz6

--- conftest.py
+++ conftest.py
@@ -1,4 +1,3 @@
-# conftest.py
 import pytest
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
@@ -7,15 +6,18 @@ from selenium.webdriver.firefox.options import Options
 @pytest.fixture(scope="function")
 def browser():
     options = Options()
-    options.add_argument("--headless")  # mode headless pour CI
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--window-size=1280,720")  # moins lourd pour CI
 
-    # Point vers geckodriver téléchargé par le workflow
     service = Service("/usr/local/bin/geckodriver")
-
     driver = webdriver.Firefox(service=service, options=options)
+
+    # Timeouts
+    driver.set_page_load_timeout(180)
+    driver.implicitly_wait(10)
+
     yield driver
     driver.quit()
